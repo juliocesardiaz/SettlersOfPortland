aba88348cdd5b547e48fb1dbafb94fef3606417d
Jeff Austin <jeffaustin81@gmail.com>
2015-10-01 15:58:00 -0700

    Add player and tile factory

---
+++ app/scripts/game/services/PlayerFactory.js
@@ -0,0 +1,6 @@
+settlers.factory('PlayerFactory', function PlayerFactory() {
+    var factory {};
+    factory.players = [
+    {name: "Player One", type: "", resources: [granola= 0, coffee = 0, beer = 0, tattoos = 0, kale = 0], victory_points = 0 }
+    ];
+});
